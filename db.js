68c5618702ed4712878af27f3678ac4f1307898e
mrjayrai <raijai127@gmail.com>
2024-06-21 16:35:03 +0530

    Added env

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 // const mongoURI = "mongodb://localhost:27017/one";
-const mongoURI = "mongodb+srv://pritesh:pritesh%40mongodb1@cluster0.pyfudfg.mongodb.net/one?retryWrites=true&w=majority";
+// const mongoURI = "mongodb+srv://pritesh:pritesh%40mongodb1@cluster0.pyfudfg.mongodb.net/one?retryWrites=true&w=majority";
+require('dotenv').config();
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('DB connected'))
         .catch(err => console.error('DB connection error:', err));
 };
