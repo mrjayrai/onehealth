18ee720271254ebe981e91d6289b112321a80aca
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2024-06-21 22:12:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb://localhost:27017/one";
-// const mongoURI = "mongodb+srv://pritesh:pritesh%40mongodb1@cluster0.pyfudfg.mongodb.net/one?retryWrites=true&w=majority";
+
 require('dotenv').config();
 
 const connectToMongo = () => {
